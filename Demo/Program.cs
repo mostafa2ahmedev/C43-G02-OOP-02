f407769e30820c559ab8dbc4d361c7020b85368a
mostafa2ahmedev <mostafaahmedev2002@gmail.com>
2025-01-02 01:41:04 +0200

    Q1 P01

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,10 +1,29 @@
-﻿namespace Demo
+﻿using System.ComponentModel;
+
+namespace Demo
 {
+    #region Q1) Create an enum called "WeekDays" with the days of the week (Monday to Sunday) as its members.Then, write a C# program that prints out all the days of the week using this enum.
+    //enum WeekDays : byte { 
+    //Monday,Tuesday,Wednesday,Thursday,Friday,Saturday,Sunday
+    //}
+    #endregion
     internal class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+
+            #region Q1) Create an enum called "WeekDays" with the days of the week (Monday to Sunday) as its members.Then, write a C# program that prints out all the days of the week using this enum.
+            //WeekDays week;
+            //for (int i = 0; i < 6; i++)
+            //{
+            //    week = (WeekDays) i;
+            //    Console.WriteLine(week);
+                
+            //}
+            #endregion
+
         }
     }
 }
